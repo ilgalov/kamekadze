bc62d13ba872c350d8e65f9c4ebe5a7d55bbca18
ilgalov <sasasazhin777@mail.ru>
2026-04-09 12:51:35 +0300

    Improving sounds
    Added reload sound and The noise level of the explosion has been reduced.

--- script/player_tnt.gd
+++ script/player_tnt.gd
@@ -9,8 +9,7 @@ func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
 		var xa = sign(global_position.x - $Marker_move.global_position.x)
 		var yu = sign($Marker_move.global_position.y - global_position.y)
 		apply_impulse(Vector2(100 * xa, -600 * yu))
-		if !$AudioStreamPlayer2D.playing:
-			$AudioStreamPlayer2D.play()
+		$Boom.play()
 		$Camera2D.shake(0.2)
 		jump_count -= 1
 	if Input.is_action_just_pressed("ui_accept") and jump_count <= 0 and !jump:
@@ -23,6 +22,8 @@ func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("floor") and jump_count < 1:
 		jump_count += 1
+		if !$reloud.playing:
+			$reloud.play()
 
 
 func _on_timer_timeout() -> void:
